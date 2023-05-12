863b49cb679461572ff53c3cd41906a9e63b9220
javohir070 <qayumovjavohir24@gmail.com>
2023-05-12 17:56:06 +0500

    bgdf

--- src/MainUrl/index.js
+++ src/MainUrl/index.js
@@ -1,2 +1,3 @@
-export const mainUrl = "http://teacher.blendedlearing.uz/api/";
+// export const mainUrl = "http://teacher.blendedlearing.uz/api/";
+export const mainUrl = "http://test3.web-projects.uz/api/";
 // export const mainUrl = "http://172.16.12.70:8000/api/";
